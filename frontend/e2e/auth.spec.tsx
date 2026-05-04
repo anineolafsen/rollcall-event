f9fa5c7f9b3f72cd928e2bc448db84efb333da61
iveroh <iveroprandheggelund@gmail.com>
2026-05-04 12:03:40 +0200

    test(e2e): Implement auth testing
    Added tests for: - Sing Up - Sign In - Delete Account

---
+++ frontend/e2e/auth.spec.tsx
@@ -0,0 +1,85 @@
+import { test, expect } from "@playwright/test";
+
+test.describe.configure({ mode: 'serial' });
+
+const user = `bob${Date.now()}+clerk_test@example.com`;
+const password = "password";
+
+test.describe("Authentication", () => {
+  test("Sign Up", async ({ page }) => {
+    await page.goto("/sign-up");
+
+    // Signup
+    await page.fill('input[type="email"]', user);
+    await page.fill('input[type="password"]', password);
+    await page.click('div[tabindex="0"]:has-text("Sign Up")');
+
+    await page.waitForTimeout(3000);
+
+    // Verification email
+    await page.fill('input[placeholder="Verification code"]', "424242");
+    await page.click('div[tabindex="0"]:has-text("Verify Email")');
+
+    await page.waitForTimeout(3000);
+
+    // Input user details
+    await page.fill('input[placeholder="First Name"]', "Bob");
+    await page.fill('input[placeholder="Last Name"]', "Smith");
+    await page.fill('input[placeholder="Phone Number"]', "12345678");
+    await page.click('div[tabindex="0"]:has-text("Continue")');
+
+    await page.waitForTimeout(3000);
+
+    // Confirm user details
+    await page.fill('input[placeholder="First Name"]', "Bob");
+    await page.fill('input[placeholder="Last Name"]', "Smith");
+    await page.fill('input[placeholder="Phone Number"]', "12345678");
+    await page.click('div[tabindex="0"]:has-text("Continue")');
+
+    // Verify that the user is signed in
+    await expect(page.locator("text=Rollcall Event")).toBeVisible();
+    await expect(page.locator("text=Trip management")).toBeVisible();
+    await expect(page.locator("text=Invitations")).toBeVisible();
+    await expect(page.locator("text=Chats")).toBeVisible();
+    await expect(page.locator("text=Profile")).toBeVisible();
+
+    // Sign out
+    await page.goto("/profile");
+    page.once("dialog", (dialog) => dialog.accept());
+    await page.click('div[tabindex="0"]:has-text("Sign out")');
+  });
+
+  test("Login", async ({ page }) => {
+    await page.goto("/sign-in");
+    await page.fill('input[type="email"]', user);
+    await page.fill('input[type="password"]', password);
+    await page.click('div[tabindex="0"]:has-text("Sign In")');
+
+    // Add this assertion
+    await expect(page.locator("text=Rollcall Event")).toBeVisible();
+    await expect(page.locator("text=Trip management")).toBeVisible();
+    await expect(page.locator("text=Invitations")).toBeVisible();
+    await expect(page.locator("text=Chats")).toBeVisible();
+    await expect(page.locator("text=Profile")).toBeVisible();
+  });
+
+  test("Delete Account", async ({ page }) => {
+    await page.goto("/sign-in");
+    await page.fill('input[type="email"]', user);
+    await page.fill('input[type="password"]', password);
+    await page.click('div[tabindex="0"]:has-text("Sign In")');
+  
+    // Delete user after
+    await page.goto("/profile");
+
+    // Listen for and accept the confirmation dialog
+    page.once("dialog", (dialog) => dialog.accept());
+
+    await page.click('div[tabindex="0"]:has-text("Delete Account")');
+    await page.waitForTimeout(3000);
+    await page.reload();
+    await expect(
+      page.locator("text=Sign in to see your trips and events"),
+    ).toBeVisible();
+  });
+});
